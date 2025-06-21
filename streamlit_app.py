9410286a479e3ed292672a8ea648e369ad5709e1
Ruthwik Jakkula <149759199+RuthwikJakkula@users.noreply.github.com>
2025-06-21 11:22:26 -0700

    updating homepage

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("📄 Document question answering")
+st.title("LabWise")
 st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
+    "Upload your lab results below and ask a question about it "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
